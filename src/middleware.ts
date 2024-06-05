bed5a409a34f265aaac1bec2962acb244c352647
ken-kobayashi <kenkoba6827@gmail.com>
2024-06-05 11:36:56 +0900

    fix パスワードリセットメールのURLを認証チェックから除外

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|admin/login|$|login|signup|reset-password|update-password|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|admin/login|$|login|signup|confirm|reset-password|update-password|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
